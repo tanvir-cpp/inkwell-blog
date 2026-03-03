68d549736c491cf784aa9e149769815675ad897c
Tanvir <tanvirrahmanacc@gmail.com>
2026-03-04 05:45:06 +0600

    Include env.js for GitHub Pages deployment (anon key is a public publishable key)

---
+++ js/env.js
@@ -0,0 +1,5 @@
+// Automatically generated from .env
+window.ENV = {
+    SUPABASE_URL: "https://wwzzafzgaelrcgfjarth.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3enphZnpnYWVscmNnZmphcnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NTkxMzEsImV4cCI6MjA4ODEzNTEzMX0.LBh7LPliaJ3y-oQglW7wiCnZlV3PqoQgdTqg6GlbGOA"
+};
